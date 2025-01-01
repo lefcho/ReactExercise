1ec5cddb73ba6390c6594c4df241d4d2a5c7900a
ivand <ivan.d.kostov@gmail.com>
2025-01-01 20:51:03 +0200

    lesson 3: styling complete

--- exercise_3/style-app/src/App.jsx
+++ exercise_3/style-app/src/App.jsx
@@ -1,4 +1,9 @@
+import Button from "./Button/Button.jsx"
+
 function App() {
+  return(
+    <Button/>
+  );
 
 }
 
---
+++ exercise_3/style-app/src/InlineButton.jsx
@@ -0,0 +1,19 @@
+function InlineButton() {
+
+    const styles = {
+            backgroundColor: "hsl(200, 100%, 50%)",
+            color: "white",
+            padding: "10px 20px",
+            borderRadius: "5px",
+            border: "none",
+            cursor: "pointer",
+    }
+
+    return(
+        <button style={styles}>
+            Click here
+        </button>
+    );
+}
+
+export default InlineButton
