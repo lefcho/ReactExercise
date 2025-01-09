85ff497fd9157d765dc16d40cc3b7f163e1ad733
ivand <ivan.d.kostov@gmail.com>
2025-01-09 20:34:27 +0200

    to do app project

--- project-1/to-do-project/src/App.jsx
+++ project-1/to-do-project/src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import ToDoInput from "./components/ToDoInput.jsx";
+import ToDoList from "./components/ToDoList.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [todos, setTodos] = useState([]);
+    const [todoValue, setTodoValue] = useState('');
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    function persistData(newList) {
+        localStorage.setItem("todos", JSON.stringify({ todos:newList }))
+    }
+
+    function handleAddTodos(newTodo) {
+        const newTodoList = [...todos, newTodo];
+        persistData(newTodoList);
+        setTodos(newTodoList);
+    }
+
+    function handleDeleteTodo(index) {
+        const newTodoList = todos.filter((_, todoindex) => {
+            return todoindex !== index;
+        })
+        persistData(newTodoList);
+        setTodos(newTodoList);
+    }
+
+    function handleEditTodo(index) {
+        const valueToBeedited = todos[index];
+        setTodoValue(valueToBeedited);
+        handleDeleteTodo(index);
+    }
+
+    useEffect(() => {
+        if (!localStorage) {
+            return;
+        } 
+        let localTodos = localStorage.getItem("todos");
+
+        if (!localTodos) {
+            return;    
+        }
+
+        localTodos = JSON.parse(localTodos).todos;
+        setTodos(localTodos);
+        
+    }, [] )
+
+    return (
+        <>
+            <ToDoInput todoValue={todoValue} setTodoValue={setTodoValue} handleAddTodos={handleAddTodos} />
+            <ToDoList handleEditTodo={handleEditTodo} handleDeleteTodo={handleDeleteTodo} todos={todos}/>
+        </>
+    )
 }
 
-export default App
+export default App;
---
+++ project-1/to-do-project/src/components/ToDoInput.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+function ToDoInput(props) {
+const { handleAddTodos, todoValue, setTodoValue } = props;
+
+    return (
+        <header>
+            <input value={todoValue} 
+                onChange={(e) => {
+                    setTodoValue(e.target.value)
+                }} 
+                placeholder="Enter a todo..." />
+            <button onClick={() => {
+                handleAddTodos(todoValue);
+                setTodoValue('');
+            }
+            }>Add</button>
+        </header>
+    )
+}
+
+export default ToDoInput;
